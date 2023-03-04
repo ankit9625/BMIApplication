8115a53044e9b0d3b2d70d25fdf9cb21ed8b3108
Ankit Kumar <ankitbabu647@gmail.com>
2023-03-04 12:31:30 +0530

    BMI Calculator UPDATED VERSION Add Some Spacial Features

--- app/src/main/java/com/example/bmiapplication/MainActivity.kt
+++ app/src/main/java/com/example/bmiapplication/MainActivity.kt
@@ -1,19 +1,29 @@
 package com.example.bmiapplication
 // Here Import Some important Packages
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.example.bmiapplication.databinding.ActivityMainBinding
 
 // This is My Main Class which will be Execute and this inherited properties of Superclass AppCompatActivity
+@Suppress("UNREACHABLE_CODE")
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     // Here I Declare ActivityMainBinding Type Reference Variable which Contain All XML File of Components of Objects or Views
     private lateinit var binding:ActivityMainBinding
     // Here I Declare isClear Variable which type is Boolean which contains only true and false value
     private var isClear=true
     // This is Create function which Create Our Activity at run time
+
+    private lateinit var builder:AlertDialog.Builder
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Here I initialize My ViewBinding reference with ActivityMainBinding Object
@@ -21,8 +31,48 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(binding.root)
         // Here I Declare Button Listener to perform task from button and Here this is Current class object
         binding.btncalclear.setOnClickListener(this)
+
+        builder=AlertDialog.Builder(this)
     }
-  // this is Overrided function which belong into AppCompatActivity Class and This is Click event function
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater:MenuInflater=menuInflater
+        inflater.inflate(R.menu.my_option_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+// I Use Option Menu Which make my Application Good and excellent
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.about_app->{
+              Toast.makeText(this," About App ",Toast.LENGTH_LONG).show()
+            }
+
+            R.id.exit->{
+                builder.setTitle("Alert!")
+                    .setMessage("Do You Want to Exit ?")
+                    .setCancelable(true)
+                    .setPositiveButton("Yes"){ _, _ ->
+                        finish()
+                    }
+                    .setNegativeButton("No"){ dialogInterface, _ ->
+                        dialogInterface.cancel()
+
+                    }
+                    .setNeutralButton("help"){ _, _ ->
+                        Toast.makeText(this,"Help",Toast.LENGTH_LONG).show()
+                    }
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    // Here I Use Code for disable back button Action
+    @Deprecated("Deprecated in Java", ReplaceWith("Unit"))
+    override fun onBackPressed() = // super.onBackPressed()
+        Unit
+
+
+    // this is Override function which belong into AppCompatActivity Class and This is Click event function
     @SuppressLint("SetTextI18n")
     override fun onClick(v: View?) {
       // Here I Take two conditions
@@ -35,8 +85,26 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             if (binding.edtinputweight.text.toString().isNotEmpty() && binding.edtinputheight.text.toString().isNotEmpty()) {
             // Here I Get input from EditText First
             val weightkg = binding.edtinputweight.text.toString().toDouble()
+                if (weightkg.toString().toDouble()==00.00) {
+                    Toast.makeText(this, " Please Enter Valid Weight ", Toast.LENGTH_LONG).show()
+                    return
+                }
+                else
+                // Here I Use Some Spacial Properties of EditText 1 Like Enabled and Clickable will be false after first time enter Value After Enter Value you Can't Change it
+              binding.edtinputweight.isEnabled=false
+                binding.edtinputweight.isClickable=false
+
+
             // here I Get input from EditText Second
             val heightcm = binding.edtinputheight.text.toString().toDouble()
+                if (heightcm.toString().toDouble()==000.00) {
+                    Toast.makeText(this, "Please Enter Valid Height ", Toast.LENGTH_LONG).show()
+                    return
+                }
+                else
+                // Here I Use Some Spacial Properties of EditText 2 Like Enabled and Clickable will be false after first time enter Value After Enter Value you Can't Change it
+                binding.edtinputheight.isEnabled=false
+                binding.edtinputheight.isClickable=false
             // Here i Perform a Function to Calculate BMI Given Inputs  and Store it in res variable which is Local type
             val res = calculateBMI(weightkg, heightcm)
             res.toString()
@@ -113,19 +181,36 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
         else {
             // This will be show when Our All Fields will be Empty
-            Toast.makeText(this, " Please Fill All Fields Very carefully ", Toast.LENGTH_LONG).show()
+            if (TextUtils.isEmpty(binding.edtinputweight.text.toString()) &&TextUtils.isEmpty(binding.edtinputheight.text.toString())) {
+                Toast.makeText(this, " Please Fill All Fields Very carefully ", Toast.LENGTH_LONG)
+                    .show()
+            }else if(TextUtils.isEmpty(binding.edtinputweight.text.toString())){
+                binding.edtinputweight.requestFocus()
+                    Toast.makeText(this, " Please Enter Weight Very Carefully ", Toast.LENGTH_LONG)
+                        .show()
+                }else if(TextUtils.isEmpty(binding.edtinputheight.text.toString())){
+                    binding.edtinputheight.requestFocus()
+                    Toast.makeText(this, " Please Enter Height Very Carefully ", Toast.LENGTH_LONG)
+                        .show()
+                }
         }
 
         }
 
         // This Block will be Call when our button perform Clear Action
         else{
-            binding.edtinputweight.setText(" ")
-            binding.edtinputheight.setText(" ")
+            binding.edtinputweight.text.clear()
+            binding.edtinputheight.text.clear()
             binding.showbmivalue.text = " "
             binding.showmessage.text = " "
             isClear=true
             binding.btncalclear.text = " CALCULATE "
+            // Here I Use Some Spacial Properties of EditText 1 Like Enabled and Clickable will be true after Action Clear You can Use again
+            binding.edtinputweight.isEnabled=true
+            binding.edtinputweight.isClickable=true
+            // Here I Use Some Spacial Properties of EditText 2 Like Enabled and Clickable will be true after Action Clear You can Use again
+            binding.edtinputheight.isEnabled=true
+            binding.edtinputheight.isClickable=true
         }
     }
 
