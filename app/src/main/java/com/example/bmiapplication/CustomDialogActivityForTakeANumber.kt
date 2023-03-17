96d4e7c9cff0fbb6b68521b970f748f62c720a2d
Ankit Kumar <ankitbabu647@gmail.com>
2023-03-17 10:22:34 +0530

    BMI Calculator UPDATED VERSION Add Some Spacial Features Add Dail Call Option 15mar Use Alert Dialog and Add Sub Menus in my BMI Application Add CustomDialogAndProgressBar 16mar Some Bug Fix 17mar

---
+++ app/src/main/java/com/example/bmiapplication/CustomDialogActivityForTakeANumber.kt
@@ -0,0 +1,11 @@
+package com.example.bmiapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomDialogActivityForTakeANumber : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_dialog_for_take_anumber)
+    }
+}
