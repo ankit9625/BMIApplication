7589b757aa7390256255c2c757be19826d8ed6cc
Ankit <ankitbabu647@gmail.com>
2023-03-19 10:39:18 +0530

    Use WebView Finally And Add This Feature in BMI 19mar

--- app/src/main/java/com/example/bmiapplication/WevViewForActivity.kt
+++ app/src/main/java/com/example/bmiapplication/WevViewForActivity.kt
@@ -1,6 +1,10 @@
 package com.example.bmiapplication
 
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bmiapplication.databinding.ActivityWevViewForBinding
 
@@ -10,17 +14,19 @@ class WevViewForActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bindingwebview= ActivityWevViewForBinding.inflate(layoutInflater)
         setContentView(bindingwebview.root)
+        supportActionBar?.hide()
+         bindingwebview.webView.webViewClient=MyWebViewClient()
          bindingwebview.webView.loadUrl("https://www.calculator.net/bmi-calculator.html")
 
     }
 
-   /* inner class MyWebViewClient :WebViewClient(){
+    inner class MyWebViewClient : WebViewClient(){
         override fun onPageFinished(view: WebView?, url: String?) {
             super.onPageFinished(view, url)
-            bindingwebview.progressbar.visibility=GONE
+            bindingwebview.progressbar.visibility=View.GONE
         }
         override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
             return super.shouldOverrideUrlLoading(view, request)
         }
-    }  */
+    }
 }
